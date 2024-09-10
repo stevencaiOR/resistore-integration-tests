690117ad42f0a0b57f47faf8996158e4ee1b34dc
stevencaiOR <sharkocai@gmail.com>
2024-09-10 13:53:41 -0700

    removed unnecessary modules

--- conftest.py
+++ conftest.py
@@ -1,9 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 
 @pytest.fixture(scope="class")
 def driver_init(request):
